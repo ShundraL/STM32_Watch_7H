4e8b7e9dc7d678e109c66156b6b28f971fd2855f
ShundraL <oleksandr.levchenko@altium.com>
2025-11-17 10:16:23 +0200

    Init routines moved from HAL to LL global_vars collects all globals

---
+++ Core/Inc/global_vars.h
@@ -0,0 +1,27 @@
+/*
+ * globals_var.h
+ *
+ *  Created on: Nov 17, 2025
+ *      Author: oleksandr.levchenko
+ */
+
+#ifndef INC_GLOBAL_VARS_H_
+#define INC_GLOBAL_VARS_H_
+#include "stm32f0xx_hal.h"
+
+
+extern uint8_t temperature_buffer[4];
+extern uint8_t display_buffer[4];
+
+extern uint16_t flags;
+extern uint8_t kbd_scan;
+extern int8_t input_position;
+
+extern int8_t second,minute,hour;
+extern int8_t interrupt_cnt;
+extern int8_t beep_duration;
+extern int8_t kbd_temp, kbd_code;
+extern int8_t setup_tmr;
+extern uint32_t system_time;
+
+#endif /* INC_GLOBAL_VARS_H_ */
---
+++ Core/Src/global_vars.c
@@ -0,0 +1,32 @@
+/*
+ * globals_var.c
+ *
+ *  Created on: Aug 13, 2025
+ *      Author: oleksandr.levchenko
+ */
+#include "main.h"
+#include "display.h"
+#include "stm32f0xx_hal.h"
+#include "global_vars.h"
+
+uint16_t flags = 0;
+uint8_t kbd_scan = 0;
+int8_t input_position = HOUR_H;
+uint8_t display_buffer[4]={0,0,0,0};
+uint8_t temperature_buffer[4]={DASH,DASH,DASH,DEGREE};
+
+int8_t second = 0;
+int8_t minute = 36 ;
+int8_t hour = 12;
+int8_t interrupt_cnt = 100;
+int8_t beep_duration;
+int8_t kbd_temp = 0;
+int8_t kbd_code = 0;
+int8_t setup_tmr = 0;
+uint32_t system_time = 0;
+
+
+
+
+
+
